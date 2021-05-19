b4e1ab1cc6ffd83bb1c07a5dcd7b9a337d77bc18
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-05-19 19:32:22 +0700

    subject

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.subscribeBy
 import io.reactivex.rxjava3.subjects.PublishSubject
 
 fun main(){
@@ -22,7 +24,26 @@ fun main(){
     subscriptionOne.dispose()
     publishSubject.onNext(4)
 
-    //1
+    //1. mengirim event complete sepanjang subject melalu method complete
+    // ini secara efective mengakhiri subject observable sequence
     publishSubject.onComplete()
+    //2. mengirim element lain 5 ke dalam subject. ini tidak akan di munculkan dan di print
+    //meskipun, karena subject sudah di terminasi
+    publishSubject.onNext(5)
+    //3. jangan pesnah lupa menutup subscription dengan dispose ketika sudah selesai ;)
+    subscriptionTwo.dispose()
+    //4. menciptakan subscription baru, akan memulai subject kembali ke aksi? oh tentu tidak '<'
+    // tetapi tetap sadja akan mendapat onComplete event '<'
+    val subscriptionThree = publishSubject.subscribeBy(
+      onNext = { printWithLabel("3)", it) },
+      onComplete = {printWithLabel("3)", "Complete")}
+    )
+
+    publishSubject.onNext(6)
+  }
+
+  //1
+  exampleOf("Behaviour Subject"){
+    val subscriptions = CompositeDisposable()
   }
 }
